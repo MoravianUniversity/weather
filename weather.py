764890602986f99e86895bb6c7fb43984e936472
Ben Coleman <colemanb@moravian.edu>
2023-02-09 07:11:26 -0500

    refactor to functions to prepare for formatting

--- weather.py
+++ weather.py
@@ -1,11 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
 
-contents = requests.get('http://forecast.weather.gov/MapClick.php?lat=40.6222&lon=-75.3932')
-soup = BeautifulSoup(contents.text, 'html.parser')
 
-forecasts = soup.find_all('img', class_='forecast-icon')
+def get_weather():
+    contents = requests.get('http://forecast.weather.gov/MapClick.php?lat=40.6222&lon=-75.3932')
+    soup = BeautifulSoup(contents.text, 'html.parser')
 
-for forecast in forecasts:
-    print(forecast['alt'])
+    forecasts = soup.find_all('img', class_='forecast-icon')
+
+    weather = []
+    for forecast in forecasts:
+        weather.append(forecast['alt'])
+    return weather
+
+
+def output_weather(weather_list):
+    for report in weather_list:
+        print(report)
+
+if __name__ == '__main__':
+    weather = get_weather()
+    output_weather(weather)
 
